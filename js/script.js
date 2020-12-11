a18eeded18927d570b12526d257738354f98138b
Davide <kalelc351k@gmail.com>
2020-12-11 15:23:36 +0100

    footer completato

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var app = new Vue({
     el: '#root',
     data: {
+        // dati per nav
         logo: 'img/dark-logo.png',
-        // arrow: '<i class="fas fa-chevron-down"></i>',
         arrayNav: [
             {
                 active: false,
@@ -70,8 +70,146 @@ var app = new Vue({
                     'Shop5'
                 ]
             }
-        ]
+        ],
+        // dati per footer
+        address: {
+            titolo: 'Address',
+            arrayAddress: [
+                '382 NE 191st # 87394 Miami, FL 33179-3899',
+                '+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)',
+                'support@maxcoach.com',
+            ]
+        },
+        explore: {
+            titolo: 'Explore',
+            arrayExplore: [
+                { 
+                    testo: 'Start here',
+                    link: '#'
+                },
+                { 
+                    testo: 'Success story',
+                    link: '#'
+                },
+                { 
+                    testo: 'Blog',
+                    link: '#'
+                },
+                { 
+                    testo: 'Courses',
+                    link: '#'
+                },
+                { 
+                    testo: 'Contact US',
+                    link: '#'
+                }
+            ]
+        },
+        info: {
+            titolo: 'Information',
+            arrayInfo: [
+                { 
+                    testo: 'Membership',
+                    link: '#'
+                },
+                { 
+                    testo: 'Purchase guide',
+                    link: '#'
+                },
+                { 
+                    testo: 'Privacy policy',
+                    link: '#'
+                },
+                { 
+                    testo: 'Terms of services',
+                    link: '#'
+                }
+            ]
+        },
+        insta: {
+            titolo: 'Instagram',
+            mail: '@maxcoach',
+            arrayInsta: [
+                {
+                    url: 'img/120084500_197897808368764_8114555493043279565_n.jpg'
+                },
+                {
+                    url: 'img/120012142_177596140500760_8623485824101406058_n.jpg'
+                },
+                {
+                    url: 'img/120099363_364334431619755_7198812647386067017_n.jpg'
+                }
+            ]
+        }
     },
+
+        // Instagram: 
+        // {
+        //     titolo: 'Instagram'
+        // }
+        // [
+        //     { 
+        //         testo: 'Membership',
+        //         link: '#'
+        //     },
+        //     { 
+        //         testo: 'Purchase guide',
+        //         link: '#'
+        //     },
+        //     { 
+        //         testo: 'Privacy policy',
+        //         link: '#'
+        //     },
+        //     { 
+        //         testo: 'Terms of services',
+        //         link: '#'
+        //     }
+        // ],
+
+
+
+
+    //     arrayFooter: [
+    //         {
+    //             key: 'Andress',
+    //             arrayKey: [
+    //                 '382 NE 191st # 87394 Miami, FL 33179-3899',
+    //                 '+1(305) 547-9909 (9am - 5pm EST, Monday - Friday)',
+    //                 'support@maxcoach.com',
+    //                 'v-html="<i class="fab fa-facebook-square"></i>"'
+    //                 // '<i class="fab fa-facebook-square"></i>   <i class="fab fa-twitter"></i>   <i class="fab fa-instagram"></i>   <i class="fab fa-linkedin"></i>'
+    //             ]
+    //         },
+    //         {
+    //             key: 'Explore',
+    //             arrayKey: [
+    //                 'Start here',
+    //                 'Success story',
+    //                 'Blog',
+    //                 'Courses',
+    //                 'Contact US'
+    //             ]
+    //         },
+    //         {
+    //             key: 'Information',
+    //             arrayKey: [
+    //                 'Membership',
+    //                 'Purchase guide',
+    //                 'Privacy policy',
+    //                 'Terms of services'
+    //             ]
+    //         },
+    //         {
+    //             key: 'Instagram @maxcoach',
+    //             arrayKey: [
+    //                 'img/120084500_197897808368764_8114555493043279565_n.jpg',
+    //                 'Image',
+    //                 'Image',
+    //             ]
+
+    //         }
+    //     ]
+    // },
     methods: {
         mostra(elementi, indice) {
             // condizione per espandere dropdown se la variabile active e' su true o false

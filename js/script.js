20ae986731afec8d6c38c1d75e51c39fc7cf1a95
Davide <kalelc351k@gmail.com>
2020-12-14 17:18:52 +0100

    fix dropdown ecc...

--- js/script.js
+++ js/script.js
@@ -2,51 +2,82 @@ var app = new Vue({
     el: '#root',
     data: {
         // dati per nav
+        // elemento attivo creato per escludere dalla chiusura il dropdown che interessa lasciare aperto
+        elementoAttivo: 0,
         logo: 'img/dark-logo.png',
         arrayNav: [
             {
                 active: false,
                 key: 'Home',
                 arrayKey: [
-                    'home1'
+                    'home 1',
+                    'home 2',
+                    'home 3',
+                    'home 4',
+                    'home 5'
                 ]
             },
             {
                 active: false,
                 key: 'Pages',
                 arrayKey: [
-                    'Pages1'
+                    'Pages 1',
+                    'Pages 2',
+                    'Pages 3',
+                    'Pages 4',
+                    'Pages 5'
                 ]
             },
             {
                 active: false,
                 key: 'Courses',
                 arrayKey: [
-                    'Courses1'
+                    'Courses 1',
+                    'Courses 2',
+                    'Courses 3',
+                    'Courses 4',
+                    'Courses 5'
                 ]
             },
             {
                 active: false,
                 key: 'Features',
                 arrayKey: [
-                    'Features1'
+                    'Features 1',
+                    'Features 2',
+                    'Features 3',
+                    'Features 4',
+                    'Features 5'
                 ]
             },            
             {
                 active: false,
                 key: 'Blog',
                 arrayKey: [
-                    'Blog1'
+                    'Blog 1',
+                    'Blog 2',
+                    'Blog 3',
+                    'Blog 4',
+                    'Blog 5'
                 ]
             },            
             {
                 active: false,
                 key: 'Shop',
                 arrayKey: [
-                    'Shop1'
+                    'Shop 1',
+                    'Shop 2',
+                    'Shop 3',
+                    'Shop 4',
+                    'Shop 5'
                 ]
             }
         ],
+        // dati per il carosello
+        mostraCarosello1: true,
+        mostraCarosello2: false,
+        newSelect1: 'select',
+        newSelect2: '',
         // dati per footer
         address: {
             titolo: 'Address',
@@ -120,12 +151,42 @@ var app = new Vue({
     },
     methods: {
         mostra(elementi, indice) {
+            // elemento attivo creato per escludere dalla chiusura il dropdown che interessa lasciare aperto
+            this.elementoAttivo = indice;
+            console.log(indice);
+            
             // condizione per espandere dropdown se la variabile active e' su true o false
             if (elementi.active == false) {
                 elementi.active = true;
             } else {
                 elementi.active = false;
             }
+            // ciclo for che permette la chiusura di altri elementi aperti, evitando aperture contemporanee di dropdown
+            this.arrayNav.forEach((element, index) => {
+                if (indice !== index) {
+                    this.arrayNav[index].active = false
+                } 
+            });
+        },
+        // chiusura del dropdown attivo cliccando sullo spazio dell'header
+        chiudi() {
+            this.arrayNav[this.elementoAttivo].active = false;
+        },
+        scroll() {
+            window.scrollTo(0, 0);
+        },
+        // meglio inserire i pulsanti i nei data di vue sotto forma di array e far scorrere la classe in base all'array
+        cambioCarosello1() {
+            this.mostraCarosello1 = true;
+            this.mostraCarosello2 = false;
+            this.newSelect1 = 'select';
+            this.newSelect2 = '';
+        },
+        cambioCarosello2() {
+            this.mostraCarosello1 = false;
+            this.mostraCarosello2 = true;
+            this.newSelect1 = '';
+            this.newSelect2 = 'select';
         }
     }
 });
